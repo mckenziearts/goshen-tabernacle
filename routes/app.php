99aa320c3fa64efdf46e6939bd4745738dfa9c21
Arthur Monney <monneylobe@gmail.com>
2021-02-22 17:52:59 +0100

    :package: ajout d'une redirection dans le fichier app.php

--- routes/app.php
+++ routes/app.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::redirect('/', '/login');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
