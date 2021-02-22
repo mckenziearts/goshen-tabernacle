57cccf29cbee7803653a5d7f2e98687010547b8a
Arthur Monney <monneylobe@gmail.com>
2021-02-22 17:51:04 +0100

    :wrench: Ajout du fichier de configuration de Goshen

---
+++ config/goshen.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App subdomain
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default subdomain  for your application.
+    | You may change these defaults as required, but they're a perfect start
+    | for this applications.
+    |
+    */
+
+    'sub_domain' => 'app.'. env('APP_DOMAIN'),
+];
