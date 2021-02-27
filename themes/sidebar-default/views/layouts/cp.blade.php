fc2dfe338f75461c10fc93f446d87f2a3931c048
Arthur Monney <monneylobe@gmail.com>
2021-02-27 07:32:08 +0100

    :lipstick: Add icon on ui dashboard layout

--- themes/sidebar-default/views/layouts/cp.blade.php
+++ themes/sidebar-default/views/layouts/cp.blade.php
@@ -8,6 +8,13 @@
     <title>@yield('title') - CPanel</title>
     <meta name="apple-mobile-web-app-title" content="{{ app_name() }}">
     <meta name="application-name" content="{{ app_name()  }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
