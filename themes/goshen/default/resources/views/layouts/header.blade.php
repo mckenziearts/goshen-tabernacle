3bc492253e42c8a496c60d3f182773d2bacfb8f3
Arthur Monney <monneylobe@gmail.com>
2021-12-23 14:11:16 +0100

    :lipstick: ajout des icons sur le header

--- themes/goshen/default/resources/views/layouts/header.blade.php
+++ themes/goshen/default/resources/views/layouts/header.blade.php
@@ -83,9 +83,7 @@
                             <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-6">
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/chart-bar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
-                                    </svg>
+                                    <x-heroicon-o-document-text class="flex-shrink-0 h-6 w-6 text-purple-600"/>
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('A propos') }}
@@ -97,9 +95,7 @@
                                 </a>
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
-                                    </svg>
+                                    <x-heroicon-o-office-building class="flex-shrink-0 h-6 w-6 text-purple-600"/>
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Construction') }}
@@ -111,9 +107,7 @@
                                 </a>
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/shield-check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
-                                    </svg>
+                                    <x-heroicon-o-calendar class="flex-shrink-0 h-6 w-6 text-purple-600" />
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Événements') }}
@@ -158,33 +152,28 @@
                             <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-6">
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
-                                    </svg>
+                                    <img class="flex-shrink-0 h-6 w-6 rounded-md" src="{{ asset('images/william-branham.jpeg') }}" alt="William Marrion Brahnam picture">
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Qui est William Marrion Brahnam ?') }}
                                         </p>
                                         <p class="mt-1 text-sm text-gray-500">
-                                            {{ __('Speak directly to your customers in a more meaningful way.') }}
+                                            {{ __('Découvrez toute la biographie du prophète et son histoire.') }}
                                         </p>
                                     </div>
                                 </a>
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/shield-check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
-                                    </svg>
+                                    <x-heroicon-o-book-open class="flex-shrink-0 h-6 w-6 text-purple-600"/>
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Brochures') }}
                                         </p>
                                         <p class="mt-1 text-sm text-gray-500">
-                                            {{ __("Your customers' data will be safe and secure.") }}
+                                            {{ __('Téléchargez les brochures pour connaitre le Message qu\'il apporte.') }}
                                         </p>
                                     </div>
                                 </a>
-
                             </div>
                         </div>
                     </div>
@@ -221,29 +210,25 @@
                             <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-6">
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
-                                    </svg>
+                                    <x-heroicon-o-video-camera class="flex-shrink-0 h-6 w-6 text-purple-600"/>
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Cultes') }}
                                         </p>
                                         <p class="mt-1 text-sm text-gray-500">
-                                            {{ __('Speak directly to your customers in a more meaningful way.') }}
+                                            {{ __('Revivez les cultes pour maintenir votre atmosphère spirituelle.') }}
                                         </p>
                                     </div>
                                 </a>
 
                                 <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                    <svg class="flex-shrink-0 h-6 w-6 text-purple-600" x-description="Heroicon name: outline/shield-check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
-                                    </svg>
+                                    <x-heroicon-o-microphone class="flex-shrink-0 h-6 w-6 text-purple-600"/>
                                     <div class="ml-4">
                                         <p class="text-base font-medium text-gray-900">
                                             {{ __('Podcasts') }}
                                         </p>
                                         <p class="mt-1 text-sm text-gray-500">
-                                            {{ __("Your customers' data will be safe and secure.") }}
+                                            {{ __('Les enseignements et études Biblique en audio.') }}
                                         </p>
                                     </div>
                                 </a>
